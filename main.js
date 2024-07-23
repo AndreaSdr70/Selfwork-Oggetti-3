4ad60175784e8779c94109f2cd458b5c1f777899
AndreaSdr70 <andrea@sdringola.it>
2024-07-23 10:03:59 +0200

    Selfwork Oggetti 3

---
+++ main.js
@@ -0,0 +1,10 @@
+DATI DI PARTENZA:
+let bowling = {
+    'players': [
+        {'name': 'Livio', 'scores': []},
+        {'name': 'Paola', 'scores': []},
+        {'name': 'Filippo', 'scores': []},
+        {'name': 'Giuseppe', 'scores': []}
+    ],
+    ...
+}
